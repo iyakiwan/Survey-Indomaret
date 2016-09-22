c13e17fa200b2a3434043ca0b5991a0e440e6550
Alie <iyakiwan19@gmail.com>
2016-09-22 10:46:04 +0700

    Mengaktifkan Onclick listener pada buttin kirim,Menambah Implemen untuk melihat banyak item yang di pilih oleh Checkbox, dan menambah valadasi bila tidak mengisi edittext

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl5024/surveyindomaret/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl5024/surveyindomaret/MainActivity.java
@@ -2,14 +2,16 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl5024.surveyindomaret;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener
 {
     EditText etComent;
     CheckBox cb1, cb2, cb3;
@@ -39,10 +41,18 @@ public class MainActivity extends AppCompatActivity
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
         bKirim = (Button) findViewById(R.id.buttonKr);
 
+        cb1.setOnCheckedChangeListener(this);
+        cb2.setOnCheckedChangeListener(this);
+        cb3.setOnCheckedChangeListener(this);
+
         kondisiawal();
-
+        bKirim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                proses();
+            }
+        });
     }
-
     private void kondisiawal()
     {
         etComent.setText("");
@@ -50,6 +60,55 @@ public class MainActivity extends AppCompatActivity
         cb2.setChecked(false);
         cb3.setChecked(false);
         rgPuas.clearCheck();
-        spIndomaret.setSelection(2);
+        spIndomaret.setSelection(0);
+    }
+    private void proses()
+    {
+        if(isValid())
+        {
+
+        }
+    }
+
+    private boolean isValid()
+    {
+        boolean valid = true;
+
+        String KoSan = etComent.getText().toString();
+
+        if (KoSan.isEmpty())
+        {
+            etComent.setError("Anda belum mengisi komentar dan saran");
+            valid =false;
+        }
+        else
+        {
+            etComent.setError(null);
+        }
+        return  valid;
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean pilih) {
+        if(pilih) Pilih+=1;
+        else Pilih-=1;
+
+        if(Pilih == 0 )
+        {
+            cbPilih = "Tidak mendapatkan";
+        }
+        else if(Pilih == 1 )
+        {
+            cbPilih = "Mendapatkan 1";
+        }
+        else if(Pilih == 2 )
+        {
+            cbPilih = "Mendapatkan 2";
+        }
+        else if(Pilih == 3 )
+        {
+            cbPilih = "Mendapatkan semua";
+        }
+
     }
 }
